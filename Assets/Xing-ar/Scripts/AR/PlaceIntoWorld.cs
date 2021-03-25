999c9177aef66e7dee73b73e59f4f8baa5f622a1
msolimando <michele.solimando@unibo.it>
2021-03-25 18:24:31 +0100

    gps placment - initial support

---
+++ Assets/Xing-ar/Scripts/AR/PlaceIntoWorld.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlaceIntoWorld : MonoBehaviour
+{
+    [Serializable]
+    /*public class ObjectUpdatedEvent : UnityEvent<GameObject, Location, int>
+    {
+    }*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
